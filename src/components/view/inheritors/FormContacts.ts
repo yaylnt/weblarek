5cd0c3f8bccf650424b5cfeb2616a71890b9205f
Yuliana Galkina <yayltw17@yandex.ru>
2026-04-06 18:27:33 +0700

    описан класс формы контактов

---
+++ src/components/view/inheritors/FormContacts.ts
@@ -0,0 +1,18 @@
+import { Form } from "../Form";
+import { ensureElement } from "../../../utils/utils";
+import { ContactsData } from "../../../types";
+import { IEvents } from "../../base/Events";
+
+export class FormContacts extends Form<ContactsData> {
+    protected emailElement: HTMLInputElement;
+    protected phoneElement: HTMLInputElement;
+
+    constructor(container: HTMLElement, events: IEvents) {
+        super(container, events);
+        this.emailElement = ensureElement<HTMLInputElement>('[name="email"]', container);
+        this.phoneElement = ensureElement<HTMLInputElement>('[name="phone"]', container);
+
+        this.container.addEventListener('submit', () => events.emit('contacts:submit'));
+        this.container.addEventListener('input', () => events.emit('contacts:change', { email: this.emailElement.value, phone: this.phoneElement.value }));
+    }
+}
